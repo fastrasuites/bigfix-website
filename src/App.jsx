ccb8ff58169098a7800d27d8682d4e9681eed582
Balikis Omolewa <omolewabalikis149@gmail.com>
2024-09-05 16:46:21 +0100

    more about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header'; 
+import MoreAbout from './components/sections/moreAbout/MoreAbout';
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* Other components */}
+      <MoreAbout />
     </div>
   );
 }
---
+++ src/components/sections/moreAbout/MoreAbout.jsx
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../../sections/moreAbout/MoreAbout.css'
+
+
+function MoreAbout() {
+  return (
+    <Container fluid className='section' >
+      <Row>
+        <Col >
+        <h3 className="title">MORE ABOUT US</h3>
+        <h3 className='titlePhrase'>
+        We provide effective and efficient <br /> support services that guarantee <br />
+         seamless operations
+        </h3>
+        <p className='desc'>
+        Bigfix Tech specializes in delivering big, powerful,and  tailored IT fixes that empower businesses <br /> 
+        to excel. From seamless ERP Implementation to rock-solid IT Infrastructure setup, we ensure <br />
+        your technology runs smoothly and drives success. Our expert Training programs give your team <br />
+        the skills they need, while our Business Automation solutions streamline your operations, fixing <br />
+        inefficiencies for peak performance. With cutting-edge Cloud Solutions and top-notch Security <br />
+        Services, we safeguard your data and fine-tune your IT environment, enabling your business 
+        to <br /> thrive in today’s digital landscape.
+        </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default MoreAbout;
