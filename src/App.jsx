89b70de79a25998e3e97033603785d840a747614
Balikis Omolewa <omolewabalikis149@gmail.com>
2024-09-06 15:46:10 +0100

    contact&footerSection

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import MoreAbout from './components/sections/moreAbout/MoreAbout';
 import Services from './components/sections/services/Services';
 import ClientTestimonialSection from './components/sections/client/clientTestimonialSection';
 import ContactForm from './components/sections/contact/ContactForm';
+import Footer from './components/Footer';
+import Copywrite from './components/Copywrite';
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
       <Services />
       <ClientTestimonialSection />
       <ContactForm />
+      {/* footer component */}
+      <Footer />
+      <Copywrite />
     </div>
   );
 }
---
+++ src/components/Copywrite.jsx
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap';
+
+function Copywrite() {
+ return (
+    <>
+    <div className='copywrite'>
+    <Container>
+        <p style={{fontSize: '16px', color: "#fff", paddingTop: "20px", fontWeight: "300"}}>© Copyright Bigfix Integrated Technologies. All Rights Reserved</p>
+    </Container>
+    </div>
+    </>
+ )
+}
+
+export default Copywrite;
