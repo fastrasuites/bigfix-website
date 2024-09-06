8ab187b3f174dd7e1fb8f2d4593c61bebb233362
oladokunlt <lukmanoladokun86@gmail.com>
2024-09-06 13:20:17 +0100

    Hero, hero cards and  fastraSuit done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import Header from './components/Header'; 
-import MoreAbout from './components/sections/moreAbout/MoreAbout';
-import Services from './components/sections/services/Services';
+import React from "react";
+import Header from "./components/Header";
+import MoreAbout from "./components/sections/moreAbout/MoreAbout";
+import Services from "./components/sections/services/Services";
+import Hero from "./components/hero/Hero";
+import Cards from "./components/hero-card/Cards";
+import HeroCard from "./components/hero-card/HeroCard";
+import Gallery from "./components/gallery/Gallery";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div style={{ position: "absolute", zIndex: "1", width: "100%" }}>
+        <Header />
+      </div>
+      <Hero />
+
+      <HeroCard />
+
+      <Gallery />
+
       {/* Other components */}
       <MoreAbout />
       <Services />
