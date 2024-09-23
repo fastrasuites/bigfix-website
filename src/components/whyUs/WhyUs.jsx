91c8f567aad10b705e97f82e9f14663e5d5f17d5
oladokunlt <lukmanoladokun86@gmail.com>
2024-09-23 10:23:15 +0100

    Updated index.

--- src/components/whyUs/WhyUs.jsx
+++ src/components/whyUs/WhyUs.jsx
@@ -4,27 +4,29 @@ import "./whyUs.css";
 const WhyUs = () => {
   return (
     <section className="py-5  why-us">
-      <div className="why-us-wrap">
-        {/* First Box with Background Image */}
+      <div className="container">
+        <div className="why-us-wrap">
+          {/* First Box with Background Image */}
 
-        <div className="bg-image">
-          <img src={fastraDashboard} alt="" />
-        </div>
+          <div className="bg-image">
+            <img src={fastraDashboard} alt="" />
+          </div>
 
-        {/* Second Box with Subtitle, Heading, Description, and Image */}
-        <div className="why-us-article">
-          <small className="why-us-subtitle">Why FASTRASUITE</small>
-          <h2 className="my-3 why-us-heading">The Benefits of FastraSuite</h2>
-          <p className="mb-4 why-us-desc">
-            Fastra integrates all your business processes into one seamless
-            system, giving you real-time insights, enhanced productivity, and
-            the agility to stay ahead in a competitive market.
-          </p>
-          <img
-            src={progressBar}
-            alt="Descriptive Alt"
-            className=" progress-bar"
-          />
+          {/* Second Box with Subtitle, Heading, Description, and Image */}
+          <div className="why-us-article">
+            <small className="why-us-subtitle">Why FASTRASUITE</small>
+            <h2 className="my-3 why-us-heading">The Benefits of FastraSuite</h2>
+            <p className="mb-4 why-us-desc">
+              Fastra integrates all your business processes into one seamless
+              system, giving you real-time insights, enhanced productivity, and
+              the agility to stay ahead in a competitive market.
+            </p>
+            <img
+              src={progressBar}
+              alt="Descriptive Alt"
+              className=" progress-bar"
+            />
+          </div>
         </div>
       </div>
     </section>
