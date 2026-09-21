760077b2b14a6740918794e500cf0c1598b0f039
SAHEED-KESLI <kasalisaheed2017@gmail.com>
2026-09-21 14:26:25 +0100

    Updated CloudOne Url of the NavDropdown link to point to correct URL

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -6,8 +6,7 @@ export const HOME_PATH = "/";
 export const ABOUT_US_PATH = "/about-us";
 export const CONTACT_PATH = "/contact";
 
-export const CLOUD_INFRASTRUCTURE_URL =
-  "https://www.cloud.enterpriseone.com.ng/";
+export const CLOUD_INFRASTRUCTURE_URL = "https://cloudone.com.ng/";
 
 /** CloudOne marketing site. */
 export const CLOUDONE_URL = "https://cloudone.com.ng";
